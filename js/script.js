8fd2b8590fd494641cace1937d0295dc1f64f487
Salvatore Tidu <salvatoret9@outlook.it>
2021-03-24 18:10:22 +0100

    JS_HTML_CSS_Boolzap_(Milestone_2 + Milestone_3_(Punto_1))

--- js/script.js
+++ js/script.js
@@ -8,100 +8,140 @@ var app = new Vue({
 
         currentContact: 0,
 
+        active_contact: 0,
+
+        userMessage: "",
+
+        user: {
+
+            name: "Salvatore",
+            avatar: "avatar_io.jpg"
+        },
+
         contacts: [
+
             {
                 name: 'Michele',
                 avatar: 'avatar_1.jpg',
                 visible: true,
                 messages: [
+
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
+
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Ricordati di dargli da mangiare',
                         status: 'sent'
                     },
+
                     {
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
                         status: 'received'
                     }
-                ],
+                ]
             },
+
             {
                 name: 'Fabio',
                 avatar: 'avatar_2.jpg',
                 visible: true,
                 messages: [
+
                     {
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
                         status: 'sent'
                     },
+
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
+
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
                     }
-                ],
+                ]
             },
+
             {
                 name: 'Samuele',
                 avatar: 'avatar_3.jpg',
                 visible: true,
                 messages: [
+
                     {
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
                         status: 'received'
                     },
+
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
+
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
                         status: 'received'
                     }
-                ],
+                ]
             },
+
             {
                 name: 'Luisa',
                 avatar: 'avatar_8.jpg',
                 visible: true,
                 messages: [
+
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
+
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
                         status: 'received'
                     }
-                ],
+                ]
             },
         ]
-        
-
-    
     },
 
     methods: {
 
-        
-    }
-})
+        changeActiveContact(index) {
+
+            this.active_contact = index;
+        },
+
+        sendMessage() {
+
+            let newUserMessage = {
+
+                date: "24/03/2021 17:53:22",
+                text: this.userMessage,
+                status: "sent"
+            };
+
+            this.contacts[this.active_contact].messages.push(newUserMessage);
+
+            this.userMessage = "";
+        }
+    },
+
+});
 
 Vue.config.devtools = true;
